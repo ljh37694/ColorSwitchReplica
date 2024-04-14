257df5cdd018b324c5729064b15df3daf91cd8da
i_junghoon <ljh37694@naver.com>
2024-04-14 22:38:12 +0900

    게임 시작시 Player의 색이 랜덤으로 변하는 기능 구현

--- ColorSwitchReplica/Assets/Scripts/Player.cs
+++ ColorSwitchReplica/Assets/Scripts/Player.cs
@@ -1,19 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 	public float jumpForce = 10f;
+	private readonly string[] colorNames = new string[4] { "Blue", "Yellow", "Red", "Violet" };
 
 	public Rigidbody2D rb;
+	public SpriteRenderer sr;
 
-    void Update()
-    {
+	private string currentColor;
+	public Color[] colors = new Color[4];
+
+	void Start() {
+		SetRandomColor();
+	}
+
+	void Update() {
         if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)) {
 
 			rb.velocity = Vector2.up * jumpForce;
 		}
     }
+
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log(col.tag);
+	}
+
+	void SetRandomColor() {
+		int idx = Random.Range(0, 3);
+
+		currentColor = colorNames[idx];
+		sr.color = colors[idx];
+	}
 }
